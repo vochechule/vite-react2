367f4354e59ea73612d72e63c6288b8c3c34eb64
DanielNovak <ndan@post.cz>
2024-09-10 11:54:54 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>Ahoj</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
